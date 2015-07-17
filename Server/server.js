4c166107b97ad516f55604656e86ffead5f46866
Dianna Faulk <diannafaulk@gmail.com>
2015-07-16 20:49:32 -0700

    Edit server

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,19 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 // var path = require('path');
 require('./powerReader.js');
-
-var powerApp = express();
+var app = express();
+var http = require('http').Server(app);
 
 var port = process.env.PORT || 3030;
-powerApp.listen(port);
-console.log('Connectus is listening on port ' + port + '...');
+// app.listen(port);
 
-module.exports = powerApp;
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(port, function(){
+  console.log('Connectus is listening on port ' + port + '...');
+});
+
+module.exports = app;
